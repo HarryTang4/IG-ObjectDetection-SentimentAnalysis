b24cabbc2518debf59fb78599c74d8bdfef1a435
HarryTang4 <harrytang45@gmail.com>
2023-09-21 11:32:57 +0100

    Removed Django secret key

--- P390/settings.py
+++ P390/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fe_6g8@86ui0slzxlw6=s68qq4u9x^rzz4(q9mv%pa5&#1z^2="
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
